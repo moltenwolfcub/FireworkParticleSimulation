5c59c87032f9940de90a7e858d9316a8664a16b7
MoltenWolfCub <oliver@tomlins.org.uk>
2022-12-26 08:50:47 +0000

    adjusted the values of physics
    balanced gravity and air resistance

--- core/src/com/moltenwolfcub/firework/util/Config.java
+++ core/src/com/moltenwolfcub/firework/util/Config.java
@@ -4,6 +4,6 @@ public class Config {
     public static final Integer WINDOW_WIDTH = 1600;
     public static final Integer WINDOW_HEIGHT = 900;
 
-    public static final Float GRAVITY = 1f;
-    public static final Float AIR_RESISTANCE = 0.9f;
+    public static final Float GRAVITY = 1.3f;
+    public static final Float AIR_RESISTANCE = 0.95f;
 }
